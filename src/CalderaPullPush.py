e1e2eceee04837bfd9da2ff2cdb83bb97de1c96e
Araraxia <jcorona.lux@gmail.com>
2024-10-04 17:06:07 -0400

    Added cronitor to monitor activity.

--- src/CalderaPullPush.py
+++ src/CalderaPullPush.py
@@ -1,29 +1,38 @@
 # V1.0 - Aria Corona
 # Script to pull Spooler data from a local Caldera server, and sending it to a dedicated webhook.
 #
-#
+
 import gc
 import requests
 from datetime import datetime
 import time
+import cronitor
 
 pullTimer = 60  # seconds
 # idPrinter1 = 'bG9jYWxob3N0OjQ1MzQzfmNhbGRlcmFyaXB-RXBzb24tU3VyZUNvbG9yLUYxMDAwMC1B'  # Epson A
 # idPrinter2 = 'bG9jYWxob3N0OjQ1MzQzfmNhbGRlcmFyaXB-RXBzb24tU3VyZUNvbG9yLUYxMDAwMC1C'  # Epson B
 idPrinter1 = 'bG9jYWxob3N0OjQ1MzQzfmNhbGRlcmFyaXB-RXBzb24tU3VyZUNvbG9yLUYxMDAwMC1E' # Epson D
 idPrinter2 = 'bG9jYWxob3N0OjQ1MzQzfmNhbGRlcmFyaXB-RXBzb24tU3VyZUNvbG9yLUYxMDAwMC1F' # Epson E
+idPrinter3 = 'bG9jYWxob3N0OjQ1MzQzfmNhbGRlcmFyaXB-RXBzb24tU3VyZUNvbG9yLUYxMDAwMC1G' # Epson F
 # ip1 = '192.168.0.39'  # Master Dell PC IP
 ip1 = '192.168.0.90' # Secondary Dell PC IP
+ip2 = '192.168.0.122' # Temporary PC for Epson F
 # ip2 = '192.168.0.151'  # Alienware, currently not relevant
 pullStore1 = {}
 pullStore2 = {}  # If adding additional printers, add more of these variables.
-webhookURL = 'webhookURL'
+pullStore3 = {}
+webhookURL = ''
 urlPrinter1 = 'http://' + ip1 + ':12340/v1/jobs?idents.device=' + idPrinter1 + '&name=Autonest*&sort=idents.internal' \
-                                                                               ':desc&state=finished'
+                                                                               ':desc&state=finished&limit=15'
 urlPrinter2 = 'http://' + ip1 + ':12340/v1/jobs?idents.device=' + idPrinter2 + '&name=Autonest*&sort=idents.internal' \
-                                                                               ':desc&state=finished'
+                                                                               ':desc&state=finished&limit=15'
+urlPrinter3 = 'http://' + ip2 + ':12340/v1/jobs?idents.device=' + idPrinter3 + '&name=Autonest*&sort=idents.internal' \
+                                                                               ':desc&state=finished&limit=15'
 loopCount = 0
-
+with open("conf/Cronitor_API_Key.txt") as file:
+    cronitor_api_key = file.read()
+cronitor.api_key = cronitor_api_key
+monitor = cronitor.Monitor('Debian10C104 Caldera API Listener')
 
 # This will soon be made redundant. We will soon skip sending data to the webhook and instead process the data and send it directly to Notion.
 # Sends pulled JSON data to the provided webhook. Bad requests will continue to retry until successful.
@@ -67,6 +76,7 @@ def getRequest(urlRequest, i):
         getReq = requests.get(urlRequest)  # Makes the get request to the Caldera API.
         getReq.raise_for_status()
     except Exception as e:
+        print(e)
         error = True
 
     # If an error is returned, wait a bit before retrying.
@@ -103,16 +113,21 @@ def pullPush(urlPrinter, lastPull):
 time.sleep(1)  # Gives a little time for the Caldera API to launch.
 gc.enable()
 # Loops endlessly, delayed by whatever pullTimer is set to. Add new lines here for new Printers
-while True:
+monitor.ping(state='run')
+while loopCount < 660:
     print(idPrinter2)
     pullStore2 = pullPush(urlPrinter2, pullStore2) 
     print(idPrinter1)
     time.sleep(1)
-    pullStore1 = pullPush(urlPrinter1, pullStore1) 
+    pullStore1 = pullPush(urlPrinter1, pullStore1)
+    print(idPrinter3)
+    pullStore3 = pullPush(urlPrinter3, pullStore3)
     print(str(datetime.now().strftime("%H:%M:%S")) + " - Sleeping Loop")
     time.sleep(pullTimer)
-    loopCount = loopCount + 1
-    if loopCount > 180:
+    loopCount += 1
+    if loopCount % 5 == 0:
+        monitor.ping()
+    if loopCount % 180 == 0:
         gc.collect()
-        loopCount = 0
         print(str(datetime.now().strftime("%H:%M:%S")) + " - It's trash day.\n")
+monitor.ping(state='complete')
