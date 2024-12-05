2055f9ffa5abfb3eb8e367309cf4ebd37a5af55d
Aria Corona <acorona@menoenterprises.com>
2024-12-05 16:59:47 -0500

    Added to start script.

---
+++ AAM_WEEKLY_INVENTORY.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd C:/Users/Meno/PycharmProjects/SpoolerProcesser
+
+# Start the Python script
+C:/ProgramData/anaconda3/python C:/Users/Meno/PycharmProjects/SpoolerProcesser/src/AAM_Weekly_Inventory_Export.py &
