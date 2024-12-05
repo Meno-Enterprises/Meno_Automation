9d08213200ec0e9abd21936e6d7c96ef8836ce5c
Aria Corona <acorona@menoenterprises.com>
2024-12-05 16:53:08 -0500

    Expanded to include data for past-due orders.

--- src/MOD_DailyReport.py
+++ src/MOD_DailyReport.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Aria Corona Sept 19th, 2024
+# Aria Corona Dec 5th, 2024
 # Daily Report script for Meno On-Demand.
 # This script will query the Meno On-Demand Notion database for all jobs created in the past week, and generate a CSV report with the following information:
 # - Total active jobs by customer
@@ -30,12 +30,19 @@ import csv, os
 
 print("Starting Daily Report...")
 notion_helper = NotionApiHelper()
-csv_directory = "output"
-csv_file_name = os.path.join(csv_directory, f"MOD_Daily_Report_{datetime.now().strftime('%Y-%m-%d')}.csv")
-os.makedirs(csv_directory, exist_ok=True)
+CSV_DIRECTORY = "output"
+CSV_FILE_NAME = os.path.join(CSV_DIRECTORY, f"MOD_Daily_Report_{datetime.now().strftime('%Y-%m-%d')}.csv")
+os.makedirs(CSV_DIRECTORY, exist_ok=True)
 
-DB_ID = "f11c954da24143acb6e2bf0254b64079"
-PROPERTY_FILTER = [r"%7CVjk", r"Ye%40l", r"Mgz%3F", r"KQKT", r"zUY%3F", r"%3AL%5EW", r"a%3Ceu", r"nNsG"]
+TODAY = datetime.now(timezone.utc)
+
+automated_emails = AutomatedEmails()
+EMAIL_CONFIG_PATH = "conf/MOD_DailyReport_Email_Conf.json"
+SUBJECT = f"MOD Daily Report {datetime.now().strftime('%m-%d-%Y')}"
+body = "Please find the attached daily report for Meno On-Demand.\n\n\nThis is an automated email, please do not reply."
+
+JOB_DB_ID = "f11c954da24143acb6e2bf0254b64079"
+
 CONTENT_FILTER = {
     "and": [
         {
@@ -65,115 +72,182 @@ CONTENT_FILTER = {
         {"property": "Quantity", "number": {"greater_than": 0}}
     ]
 }
-today = datetime.now(timezone.utc)
-customer_dict = {}
-product_dict = {}
+
+
+
 job_id = []
 status_count_dict = {"Queued": 0, "Nest": 0, "Print": 0, "Production": 0, "Packout": 0, "Complete": 0, "Canceled": 0}
-total_jobs = 0  # I could do a len(notion_response) but I want to be explicit to check for any errors.
-total_items = 0 # I could do a sum of the product_quantity but I want to be explicit to check for any errors.
+LATE_JOBS = []
+DAYS_CONSIDERED_LATE = 3
 
-print("Querying Notion API...")
-notion_response = notion_helper.query("f11c954da24143acb6e2bf0254b64079", PROPERTY_FILTER, CONTENT_FILTER)
+def process_response(notion_response):
+    customer_dict = {}
+    product_dict = {}
+    total_jobs = 0
+    total_items = 0
+    
+    print("Processing Notion API response...")
+    for page in notion_response:
+        jid = page["properties"]["ID"]["unique_id"]["number"]
+        try:   
+            customer = page["properties"]["Customer"]["formula"]["string"]
+            job_status = page["properties"]["Job status"]["select"]["name"]
 
-print("Processing Notion API response...")
-for page in notion_response:
-    jid = page["properties"]["ID"]["unique_id"]["number"]
-    try:    # Jobs are supposed to have each of these properties, so this wlil filter out any jobs that are missing properties. This typically means the job was created, but not accepted in the system for various reasons.
-        customer = page["properties"]["Customer"]["formula"]["string"]
-        job_status = page["properties"]["Job status"]["select"]["name"]
+            if customer and jid not in job_id:
+                job_id.append(jid)
+                page_id = page["id"]
+                status_count_dict[job_status] += 1
+                if job_status != "Canceled":
+                    product_description = notion_helper.return_property_value(page["properties"]["Product Description"], page_id)
+                    created = datetime.fromisoformat(page["created_time"].replace('Z', '+00:00'))
+                    last_edited = datetime.fromisoformat(page["last_edited_time"].replace('Z', '+00:00'))
+                    reprint_count = notion_helper.return_property_value(page["properties"]["Reprint count"], page_id)
+                    product_quantity = notion_helper.return_property_value(page["properties"]["Quantity"], page_id)
+                    product_id = notion_helper.return_property_value(page["properties"]["Product ID"], page_id)
+                    system_status = notion_helper.return_property_value(page['properties']['System status'], page_id)
+                    order_id = notion_helper.return_property_value(page['properties']['Order'], page_id)
 
-        if customer and jid not in job_id:
-            job_id.append(jid)
-            status_count_dict[job_status] += 1
-            if job_status != "Canceled":
-                product_description = page["properties"]["Product Description"]["formula"]["string"]
-                created = datetime.fromisoformat(page["created_time"].replace('Z', '+00:00'))
-                last_edited = datetime.fromisoformat(page["last_edited_time"].replace('Z', '+00:00'))
-                reprint_count = page["properties"]["Reprint count"]["formula"]["number"]
-                product_quantity = page["properties"]["Quantity"]["number"]
-                product_id = page["properties"]["Product ID"]["formula"]["string"]
+                    # Calculate job_age excluding weekends
+                    job_age = 0
+                    created_date = created
+                    while (TODAY-created_date).days > 0:
+                        if created_date.weekday() < 5:  # Monday to Friday are counted
+                            job_age += 1
+                        created_date += timedelta(days=1)
 
-                # Calculate job_age excluding weekends
-                job_age = 0
-                created_date = created
-                while (today-created_date).days > 0:
-                    if created_date.weekday() < 5:  # Monday to Friday are counted
-                        job_age += 1
-                    created_date += timedelta(days=1)
+                    age_label = f"Day {job_age}" if job_age < 6 else "Day 6+"
+                    shipped_today = True if (job_status == "Complete") and ((TODAY - last_edited).days == 0) else False
+                    
+                    if job_age > DAYS_CONSIDERED_LATE and order_id:
+                        if order_id[0] not in LATE_JOBS:
+                            LATE_JOBS.append(order_id[0])
 
-                age_label = f"Day {job_age}" if job_age < 6 else "Day 6+"
-                shipped_today = True if (job_status == "Complete") and ((today - last_edited).days == 0) else False
+                    if customer not in customer_dict:
+                        print(f"New customer found: {customer}")
+                        customer_dict[customer] = {
+                            "Total Jobs": 0,
+                            "Day 0": 0,
+                            "Day 1": 0,
+                            "Day 2": 0,
+                            "Day 3": 0,
+                            "Day 4": 0,
+                            "Day 5": 0,
+                            "Day 6+": 0,
+                            "Shipped Today": 0
+                        }
 
-                if customer not in customer_dict:
-                    print(f"New customer found: {customer}")
-                    customer_dict[customer] = {
-                        "Total Jobs": 0,
-                        "Day 0": 0,
-                        "Day 1": 0,
-                        "Day 2": 0,
-                        "Day 3": 0,
-                        "Day 4": 0,
-                        "Day 5": 0,
-                        "Day 6+": 0,
-                        "Shipped Today": 0
-                    }
+                    if product_id not in product_dict:
+                        print(f"New product found: {product_id}")
+                        product_dict[product_id] = {
+                            "Total Items": 0,
+                            "Day 0": 0,
+                            "Day 1": 0,
+                            "Day 2": 0,
+                            "Day 3": 0,
+                            "Day 4": 0,
+                            "Day 5": 0,
+                            "Day 6+": 0,
+                            "Total Reprints": 0
+                        }   
+                    if shipped_today:
+                        customer_dict[customer]["Shipped Today"] += 1
 
-                if product_id not in product_dict:
-                    print(f"New product found: {product_id}")
-                    product_dict[product_id] = {
-                        # "Total Jobs": 0,
-                        "Total Items": 0,
-                        "Day 0": 0,
-                        "Day 1": 0,
-                        "Day 2": 0,
-                        "Day 3": 0,
-                        "Day 4": 0,
-                        "Day 5": 0,
-                        "Day 6+": 0,
-                        "Total Reprints": 0
-                    }   
-                if shipped_today:
-                    customer_dict[customer]["Shipped Today"] += 1
+                    if job_status != "Complete":
+                        total_jobs += 1
+                        total_items += product_quantity
+                        customer_dict[customer]["Total Jobs"] += 1
+                        customer_dict[customer][age_label] += 1
+                        product_dict[product_id]["Total Items"] += product_quantity
+                        product_dict[product_id][age_label] += 1
+                        product_dict[product_id]["Total Reprints"] += reprint_count
+        except Exception as e:
+            print(f"Error processing job {jid}: {e}")
+            
+    return product_dict, customer_dict, status_count_dict, total_jobs, total_items
 
-                if job_status != "Complete":
-                    total_jobs += 1
-                    total_items += product_quantity
-                    customer_dict[customer]["Total Jobs"] += 1
-                    customer_dict[customer][age_label] += 1
-                    # product_dict[product_id]["Total Jobs"] += 1
-                    product_dict[product_id]["Total Items"] += product_quantity
-                    product_dict[product_id][age_label] += 1
-                    product_dict[product_id]["Total Reprints"] += reprint_count
-    except Exception as e:
-        print(f"Error processing job {jid}: {e}")
 
-print(f"Processing finished.\nWriting to CSV {csv_file_name}...")
-with open(csv_file_name, mode='w', newline='') as csv_file:
-    csv_writer = csv.writer(csv_file)
+def get_order_list(jobs_list):
+    order_id_list = []
+    
+    for id in jobs_list:
+        page_data = notion_helper.get_page(id)
+        
+        if not page_data:
+            print(f"Error retrieving page data for job {id}")
+            continue
+        
+        order_id = notion_helper.return_property_value(page_data['properties']['Order number'], id)
+        ship_date = notion_helper.return_property_value(page_data['properties']['Ship date'], id)
+        customer = notion_helper.return_property_value(page_data['properties']['Customer'], id)
+        products = notion_helper.return_property_value(page_data['properties']['Products'], id).replace(',',' ')
+        
+        order_id_list.append((order_id, ship_date, customer, products))
+        
+    return order_id_list
+
+
+def write_csv_jobs_by_cust(csv_writer, customer_dict, total_jobs):
     header = ["Customer", "Total Active Jobs", "Day 0", "Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6+", "Shipped Today"]
     csv_writer.writerow(header)
     for customer, data in customer_dict.items():
         row = [customer] + [data["Total Jobs"], data["Day 0"], data["Day 1"], data["Day 2"], data["Day 3"], data["Day 4"], data["Day 5"], data["Day 6+"], data["Shipped Today"]]
         csv_writer.writerow(row)
     csv_writer.writerow(["Total Jobs", total_jobs])
-    csv_writer.writerow([])
+
+
+def write_csv_jobs_by_product(csv_writer, product_dict, total_items):
     csv_writer.writerow(["Product ID", "Total Active Items",  "Day 0", "Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6+", "Total Reprints"])
     for product_id, data in product_dict.items():
         row = [product_id] + [data["Total Items"], data["Day 0"], data["Day 1"], data["Day 2"], data["Day 3"], data["Day 4"], data["Day 5"], data["Day 6+"], data["Total Reprints"]]
         csv_writer.writerow(row)
     csv_writer.writerow(["Total Items", total_items])
-    csv_writer.writerow([])
+
+
+def write_csv_status_count(csv_writer, status_count_dict):
     csv_writer.writerow(["Job Status", "Jobs Count"])
     for status, count in status_count_dict.items():
         csv_writer.writerow([status, count])
-print("CSV written successfully as ", csv_file_name)
 
-print("Preparing to send email...")
-automated_emails = AutomatedEmails()
-email_config_path = "conf/MOD_DailyReport_Email_Conf.json"
-attachment_path = [csv_file_name]
-subject = f"MOD Daily Report {datetime.now().strftime('%m-%d-%Y')}"
-body = "Please find the attached daily report for Meno On-Demand.\n\n\nThis is an automated email being sent on behalf of Aria Corona, please do not reply. If you have any questions or concerns, please contact Aria directly at acorona@menoenterprises.com."
-#automated_emails.send_email(email_config_path, subject, body, attachment_path)
-print("End of script.") 
+
+def write_csv_late_orders(csv_writer, order_list):
+    csv_writer.writerow(['Orders older than 3 days.'])
+    csv_writer.writerow(["Order ID", "Ship Date", "Customer", "Products"])
+    for order_id, ship_date, customer, products in order_list:
+        csv_writer.writerow([order_id, ship_date, customer, products])
+
+
+def write_csv(customer_dict, product_dict, status_count_dict, order_list, total_jobs, total_items):
+    print(f"Processing finished.\nWriting to CSV {CSV_FILE_NAME}...")
+    with open(CSV_FILE_NAME, mode='w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+
+        write_csv_jobs_by_cust(csv_writer, customer_dict, total_jobs)
+        
+        csv_writer.writerow([])
+        
+        write_csv_jobs_by_product(csv_writer, product_dict, total_items)
+
+        csv_writer.writerow([])
+        
+        write_csv_status_count(csv_writer, status_count_dict)  
+        
+        csv_writer.writerow([])
+        
+        write_csv_late_orders(csv_writer, order_list)  
+    
+
+def main():
+    notion_response = notion_helper.query(JOB_DB_ID, content_filter=CONTENT_FILTER)
+
+    product_dict, customer_dict, status_count_dict, total_jobs, total_items = process_response(notion_response)
+
+    order_list = get_order_list(LATE_JOBS)
+
+    write_csv(customer_dict, product_dict, status_count_dict, order_list, total_jobs, total_items)
+    
+    #automated_emails.send_email(email_config_path, subject, body, [CSV_FILE_NAME])
+    
+    print("End of script.") 
+    
+if __name__ == "__main__":
+    main()
